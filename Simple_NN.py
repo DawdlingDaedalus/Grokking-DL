bf0716e59de7b59f8e8a215aad48a1fd213b990c
Peter McMaster <39925732+DawdlingDaedalus@users.noreply.github.com>
2023-04-06 20:46:16 -0700

    Create Simple_NN.py

---
+++ Simple_NN.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+weights = np.array([0.5,0.48,-0.7]) # Change these to observe behavior
+alpha = 0.1
+
+streetlights = np.array( [[ 1, 0, 1 ],
+                          [ 0, 1, 1 ],
+                          [ 0, 0, 1 ],
+                          [ 1, 1, 1 ],
+                          [ 0, 1, 1 ],
+                          [ 1, 0, 1 ] ] )
+
+walk_vs_stop = np.array( [ 0, 1, 0, 1, 1, 0 ] )
+
+input = streetlights[0]
+goal_prediction = walk_vs_stop[0]
+
+for iteration in range(40):
+    error_for_all_lights = 0
+    for row_index in range(len(walk_vs_stop)): # Breakpoint
+        input = streetlights[row_index]
+        goal_prediction = walk_vs_stop[row_index]
+
+        prediction = input.dot(weights)
+
+        error = (goal_prediction - prediction) ** 2
+        error_for_all_lights += error
+
+        delta = goal_prediction - prediction # swapped
+        weights = weights - (alpha * (input * delta))
+        print("Prediction:" + str(prediction))
+    print("Error:" + str(error_for_all_lights) + "\n")
